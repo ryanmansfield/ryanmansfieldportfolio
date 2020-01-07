33caf6f4f51c8f02134392af7355082294537eff
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-01-07 00:57:48 -0600

    content and styling transfered

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'contacts/new'
   get 'contacts/create'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'contacts#new'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/contact',     to: 'contacts#new',             via: 'get'
