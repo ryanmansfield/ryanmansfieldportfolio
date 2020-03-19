557b88a52d1c5b1520a05bb3a657f473fc98f88a
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-03-18 21:46:47 -0500

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'contacts/new'
   get 'contacts/create'
   devise_for :users
-  root to: 'contacts#new'
+  root to: 'pages#home'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/contact',     to: 'contacts#new',             via: 'get'
