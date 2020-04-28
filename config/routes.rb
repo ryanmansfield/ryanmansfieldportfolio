dd8398cc7daf35d7144d89394ac1676dd717d3d3
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-04-28 00:16:54 -0500

    Contact form added

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'contacts/new'
-  # get 'contacts/create'
-  # match '/contact',     to: 'contacts#new',             via: 'get'
-  # resources "contacts", only: [:new, :create]
-
+  resources "contacts", only: [:new, :create]
 end
